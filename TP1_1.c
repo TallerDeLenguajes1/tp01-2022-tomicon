d93060e9a29b64fc2b8d1b6a4e70d86f7faf79cf
tomicon <tomicon01@gmail.com>
2022-03-26 12:43:09 -0300

    trabajo con punteros

--- TP1_1.c
+++ TP1_1.c
@@ -2,5 +2,13 @@
 
 int main(){
 printf("hola mundo");
+int variable;
+int *pvariable= &variable;
+printf("El contenido del puntero es: %p\n", pvariable);
+printf("La dirección de memoria almacenada por el puntero es: %p\n", pvariable);
+printf("la dirección de memoria de la variable es: %p\n", pvariable);
+printf("la dirección de memoria del puntero es: %p\n", &pvariable);
+printf("el tamaño de memoria utilizado por la variable es %d", sizeof(int));
+
 return 0;
 }
